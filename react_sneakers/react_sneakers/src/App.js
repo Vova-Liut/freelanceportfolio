5a32942bb4dc0e8a29bbe0726780759ead3a1b43
Vova_Liutkevych <lv0014041@gmail.com>
2023-04-03 00:22:14 +0300

    Started react components + scss components module

--- react_sneakers/react_sneakers/src/App.js
+++ react_sneakers/react_sneakers/src/App.js
@@ -4,15 +4,36 @@ import Card from './components/Card';
 import Drawer from './components/Drawer';
 import Header from './components/Header';
 
+const arr = [
+    {
+        title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+        price: 12999,
+        imageUrl: '/img/sneakers/1.jpg'
+    },
+    {
+        title: 'Мужские Кроссовки Nike Air Max 270',
+        price: 11500,
+        imageUrl: '/img/sneakers/2.jpg'
+    },
+    {
+        title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+        price: 10500,
+        imageUrl: '/img/sneakers/3.jpg'
+    },
+    {
+        title: 'Кроссовки Puma X Aka Boku Future Rider',
+        price: 13500,
+        imageUrl: '/img/sneakers/4.jpg'
+    }
+]
+
 function App() {
     return (
         <div className="wrapper clear">
 
             <Drawer />
 
-
             <Header />
-
 
             <div className="content p-40">
                 <div className="d-flex align-center mb-40 justify-between">
@@ -24,8 +45,15 @@ function App() {
                 </div>
 
                 <div className="d-flex">
-                    <Card />
-
+                    {
+                        arr.map((item) => (
+                            <Card 
+                                title={item.title} 
+                                price={item.price} 
+                                imageUrl={item.imageUrl}
+                            />
+                        ))
+                    }
                 </div>
             </div>
         </div>
--- react_sneakers/react_sneakers/src/components/Card/index.js
+++ react_sneakers/react_sneakers/src/components/Card/index.js
@@ -1,20 +1,24 @@
 
+import styles from './card.module.scss';
 
-function Card()  {
+function Card(props)  {
+    const onClickButton = () => {
+        console.log('click');
+    }
     return(
-        <div className="card">
-            <div className="favorite">
+        <div className={styles.card}>
+            <div className={styles.favorite}>
                 <img src="/img/heart-unliked.svg" alt="Unliked" />
             </div>
-            <img widrh={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <img widrh={133} height={112} src={props.imageUrl} alt="Sneakers" />
+            <h5>{props.title}</h5>
             <div className="d-flex justify-between align-center">
 
                 <div className="d-flex flex-column">
                     <span>Цена:</span>
-                    <b>12 999 uah.</b>
+                    <b>{props.price} uah.</b>
                 </div>
-                <button className="button">
+                <button className="button" onClick={() => onClickButton()}>
                     <img width={11} height={11}  src="/img/plus.svg" alt="plus" />
                 </button>
             </div>
